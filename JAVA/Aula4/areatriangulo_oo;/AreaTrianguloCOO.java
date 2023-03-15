ba45963d2373e0f8fb70ecbe2fda569a2f0c1855
José Fábio Guimarães de Assis Filho <71357050+JoseFabioGuimaraes@users.noreply.github.com>
2023-03-14 21:26:44 -0300

    Add files via upload

---
+++ JAVA/Aula4/areatriangulo_oo;/AreaTrianguloCOO.java
@@ -0,0 +1,16 @@
+package areatriangulo_oo;
+
+import entidades.TrianguloCOO;
+
+// COM ORIENTAÇÃO A OBJETOS
+public class AreaTrianguloCOO {
+     public static void main(String[] args) {
+
+         TrianguloCOO tri = new TrianguloCOO(3.0,4.0,5.0);
+
+         System.out.println("O valor de P: " + tri);
+
+
+     }
+
+}
