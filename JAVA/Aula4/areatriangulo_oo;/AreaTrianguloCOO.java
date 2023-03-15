70fbeb4cda0e424e7ac0a606edf80241f93359a4
José Fábio Guimarães de Assis Filho <71357050+JoseFabioGuimaraes@users.noreply.github.com>
2023-03-14 21:33:18 -0300

    Update AreaTrianguloCOO.java

--- JAVA/Aula4/areatriangulo_oo;/AreaTrianguloCOO.java
+++ JAVA/Aula4/areatriangulo_oo;/AreaTrianguloCOO.java
@@ -8,7 +8,18 @@ public class AreaTrianguloCOO {
 
          TrianguloCOO tri = new TrianguloCOO(3.0,4.0,5.0);
 
-         System.out.println("O valor de P: " + tri);
+         System.out.println("O valor de P: " + tri.calcularP());
+         System.out.println("A areá é:" + tri.calcularArea());
+
+         TrianguloCOO tri2 =  new TrianguloCOO(7.5,4.5,4.02);
+         System.out.println("O valor de P do segundo triangulo é: "+ tri2.calcularP());
+         System.out.println("O valor da Area do segundo triangulo é: "+tri2.calcularArea());
+
+         if(tri.calcularArea()>tri2.calcularArea()){
+             System.out.println("A area do primeiro é maior do que o do segundo");
+         } else{
+             System.out.println("A area do segundo é maior do que o do primeiro");
+         }
 
 
      }
