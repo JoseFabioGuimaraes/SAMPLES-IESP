b7504fd24bb31e93ea17f3a3a2951de3e3659f4f
José Fábio Guimarães de Assis Filho <71357050+JoseFabioGuimaraes@users.noreply.github.com>
2023-03-16 19:51:16 -0300

    Add files via upload

---
+++ JS/array.js
@@ -0,0 +1,9 @@
+var alunos= [];
+console.log(alunos.length);
+var primeiroA = alunos[0];
+var i = 0
+while (i<5){
+    alunos[i]= prompt("Digite o nome separadamente: ");
+    i++; 
+}
+console.log(alunos)
