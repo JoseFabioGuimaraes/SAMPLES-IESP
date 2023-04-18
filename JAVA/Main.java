ebd2357687c33c79dd825472ef5e44be1e2ea5ff
José Fábio Guimarães de Assis Filho <71357050+JoseFabioGuimaraes@users.noreply.github.com>
2023-04-18 20:20:39 -0300

    Add files via upload

---
+++ JAVA/Main.java
@@ -0,0 +1,36 @@
+package app;
+import entidades.ContaCorrente;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Criando uma instância de ContaCorrente
+        ContaCorrente conta = new ContaCorrente(12345, "João da Silva");
+        String token = conta.gerarToken();
+        System.out.println("Valor do token para as proximas ações: " +token);
+
+        // Imprimindo os atributos da conta
+        System.out.println("Número da conta: " + conta.getNumero());
+        System.out.println("Titular da conta: " + conta.getTitular());
+
+        // Pedindo ao usuário para fornecer o novo número da conta e o novo nome do titular
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Novo número da conta: ");
+        int novoNumero = scanner.nextInt();
+        System.out.print("Novo nome do titular: ");
+        scanner.nextLine(); // consumindo a quebra de linha após o nextInt()
+        String novoTitular = scanner.nextLine();
+
+        // Pedindo ao usuário para fornecer o token para autenticação
+        System.out.print("Digite o token para autenticação: ");
+        token = scanner.nextLine();
+
+        // Atualizando a conta com o novo número e titular, usando o token para autenticação
+        conta.atualizarConta(novoNumero, novoTitular, token);
+
+        // Imprimindo os atributos atualizados da conta
+        System.out.println("Número da conta atualizado: " + conta.getNumero());
+        System.out.println("Titular da conta atualizado: " + conta.getTitular());
+    }
+}
