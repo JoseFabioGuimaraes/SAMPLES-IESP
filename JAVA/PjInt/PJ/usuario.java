0f91a0a10f8640faaa0cde1bd9d094cdf93ff418
José Fábio Guimarães de Assis Filho <71357050+JoseFabioGuimaraes@users.noreply.github.com>
2023-04-25 19:01:14 -0300

    Add files via upload

---
+++ JAVA/PjInt/PJ/usuario.java
@@ -0,0 +1,20 @@
+public class usuario {
+    String nome_do_usuario;
+    String email_do_usuario;
+    int senha;
+    String lista_de_favoritos;
+
+    void nome_do_usuario(String nome_do_usuario){
+        nome_do_usuario = nome_do_usuario;
+    }
+    void email_do_usuario(String email_do_usuario){
+        email_do_usuario = email_do_usuario;
+    }
+    void senha(int senha){
+        senha = senha;
+    }
+    void lista_de_favoritos(String lista_de_favoritos){
+        lista_de_favoritos = lista_de_favoritos;
+    }
+
+}
---
+++ JAVA/PjInt/PJ/video.java
@@ -0,0 +1,29 @@
+public class video {
+    String titulo;
+    String diretor;
+    String elenco;
+    String genero;
+    String sinopse;
+    int classificação_indicativa;
+    int ano_de_lançamento;
+
+    void titulo_do_filme(String titulo){
+        titulo = titulo;
+    }
+    void diretor_do_filme(String diretor){
+        diretor = diretor;
+    }
+    void elenco_do_filme(String elenco){
+        elenco = elenco;
+    }
+    void genero_do_filme(String genero){
+        genero = genero;
+    }
+    void classificação_indicativa(String classificação_indicativa){
+        classificação_indicativa = classificação_indicativa;
+    }
+    void ano_de_lançamento(String ano_de_lançamento){
+        ano_de_lançamento = ano_de_lançamento;
+    }
+
+    }
