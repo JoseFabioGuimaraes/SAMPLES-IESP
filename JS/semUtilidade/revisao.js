8fc7becf16f7f3ab2c889aef1ce6ba9ab244cd60
José Fábio Guimarães de Assis Filho <71357050+JoseFabioGuimaraes@users.noreply.github.com>
2024-03-05 15:10:39 -0300

    Create revisao.js

---
+++ JS/semUtilidade/revisao.js
@@ -0,0 +1,12 @@
+var nome = "José";
+const cpf = "123-456-789.00";
+
+function definirSobrenome(){
+    let sobreNome = "Silva";
+    return sobreNome;
+}
+
+let sobrenome = definirSobrenome();
+
+console.log(nome +"\n" +sobrenome+"\n" + cpf)
+
